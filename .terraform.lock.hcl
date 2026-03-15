b417e828314f953b8e70b496c93279c0b7c84521
mfahad2907 <mfahad2907@gmail.com>
2026-03-15 20:44:16 +0100

    working tofu 3 vms main.tf

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -1,25 +1,24 @@
 # This file is maintained automatically by "tofu init".
 # Manual edits may be lost in future updates.
 
-provider "registry.opentofu.org/bpg/proxmox" {
-  version     = "0.70.0"
-  constraints = "0.70.0"
+provider "registry.opentofu.org/telmate/proxmox" {
+  version     = "3.0.2-rc07"
+  constraints = "3.0.2-rc07"
   hashes = [
-    "h1:Vq+frhavl4SIBtt7BqGJ4XNjzvWl2lAhA0feK5i4ocg=",
-    "zh:05fe0d04fa07c6f23028e08ebf14ed01f8cb6295c926bdc923b333c1b82263e7",
-    "zh:2507482294e544990946c4ae660843c3c783dca0a104ba41156dadd9f99a6c03",
-    "zh:3b34b8af4890a7c9785335e5367b96a5f2bacd39718ae242e392a2f476cdb47b",
-    "zh:4c81279f32dee51ba760cdee546300c8bf5a00b1be8bafe0fe374e5aeedf38bd",
-    "zh:6e91c968de930e6f1f234019426e5afdfdfbbf40df6cd552708224737ef6fcf4",
-    "zh:8a4fbe6b16c216d3bc54d09eff3fc2812d9ae80fb612361428609fdff4763054",
-    "zh:95c3784147c61ad938781dac3977660882c3018af98371583a55c3dbc5e007ed",
-    "zh:9f781c90ec3e23b3aac9f7a08ba8b44f2909a65e8803ca3a034e8d138b414b4e",
-    "zh:a9e8cbfc490a7ea41e0942498604e5b709ca8967fc38ee934cde23d7aed9762a",
-    "zh:addeab5eb77b10f7d68df07919ca007acbc00ad71f256982d485bdc0a13d0191",
-    "zh:ca111d70f0f365d4b406ff0a0a3477217538a1d1cdd004e8bacfab63af23a83e",
-    "zh:d304a0a7334dce31d66eeaecd67607d2b84b7b37b2615335351268acd6cfd41f",
-    "zh:e3a5e5ec75262320b14988d18f26acefe1e75a6da6a4509cfe5edc707527e465",
-    "zh:f26e0763dbe6a6b2195c94b44696f2110f7f55433dc142839be16b9697fa5597",
-    "zh:f770d33d287837b6295dc98165e24631f8f683e5d0e5f1a0200f30edc078f516",
+    "h1:zp5hpQJQ4t4zROSLqdltVpBO+Riy9VugtfFbpyTw1aM=",
+    "zh:2ee860cd0a368b3eaa53f4a9ea46f16dab8a97929e813ea6ef55183f8112c2ca",
+    "zh:415965fd915bae2040d7f79e45f64d6e3ae61149c10114efeac1b34687d7296c",
+    "zh:6584b2055df0e32062561c615e3b6b2c291ca8c959440adda09ef3ec1e1436bd",
+    "zh:65dcfad71928e0a8dd9befc22524ed686be5020b0024dc5cca5184c7420eeb6b",
+    "zh:7253dc29bd265d33f2791ac4f779c5413f16720bb717de8e6c5fcb2c858648ea",
+    "zh:7ec8993da10a47606670f9f67cfd10719a7580641d11c7aa761121c4a2bd66fb",
+    "zh:999a3f7a9dcf517967fc537e6ec930a8172203642fb01b8e1f78f908373db210",
+    "zh:a50e6df7280eb6584a5fd2456e3f5b6df13b2ec8a7fa4605511e438e1863be42",
+    "zh:b25b329a1e42681c509d027fee0365414f0cc5062b65690cfc3386aab16132ae",
+    "zh:c028877fdb438ece48f7bc02b65bbae9ca7b7befbd260e519ccab6c0cbb39f26",
+    "zh:cf0eaa3ea9fcc6d62793637947f1b8d7c885b6ad74695ab47e134e4ff132190f",
+    "zh:d5ade3fae031cc629b7c512a7b60e46570f4c41665e88a595d7efd943dde5ab2",
+    "zh:f388c15ad1ecfc09e7361e3b98bae9b627a3a85f7b908c9f40650969c949901c",
+    "zh:f415cc6f735a3971faae6ac24034afdb9ee83373ef8de19a9631c187d5adc7db",
   ]
 }
--- main.tf
+++ main.tf
@@ -1,92 +1,77 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "bpg/proxmox"
-      version = "0.70.0"
+      source  = "Telmate/proxmox"
+      version = "3.0.2-rc07"
     }
+
   }
 }
 
 provider "proxmox" {
-  endpoint  = var.proxmox_endpoint
-  api_token = var.proxmox_api_token
-  insecure  = true
-  ssh {
-    agent    = true
-    username = "root"
-  }
+  pm_tls_insecure = true
+  pm_api_url      = "https://192.168.80.14:8006/api2/json"
+  pm_api_token_secret  = "1c442791-b012-441c-b0da-96215479e2a9"
+  pm_api_token_id = "terraform@pve!provisioner"
 }
 
-# --- TEMPLATE SECTION ---
-resource "proxmox_virtual_environment_download_file" "ubuntu_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = var.proxmox_node_name
-  file_name    = "ubuntu-24.04-cloud.img"
-  url          = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
-}
+resource "proxmox_vm_qemu" "cloudinit-test" {
+  count = 3
+  vmid = "20${count.index}"
+  name        = "terraform-test-vm-${count.index}"
+  description = "A test for using terraform and cloudinit"
 
-resource "proxmox_virtual_environment_vm" "k0s_template" {
-  name      = "k0s-template"
-  node_name = var.proxmox_node_name
-  vm_id     = 900
-  template  = true
+  # Node name has to be the same name as within the cluster
+  # this might not include the FQDN
+  target_node = "server04"
 
-  disk {
-    datastore_id = "local-lvm"
-    file_id      = proxmox_virtual_environment_download_file.ubuntu_image.id
-    interface    = "virtio0"
-    size         = 20
+
+  # The template name to clone this vm from
+  clone = "k0s-template"
+  agent = 1
+
+  os_type = "cloud-init"
+
+  cpu {
+    cores   = 2
+    sockets = 1
+    type    = "host"
   }
+  memory = 2048
 
-  initialization {
-    user_account {
-      username = "ubuntu"
-      keys     = [var.ssh_public_key]
-    }
-  }
-
-  network_device {
-    bridge = var.network_bridge
-  }
-}
-
-# --- NODES SECTION ---
-resource "proxmox_virtual_environment_vm" "k0s_nodes" {
-  count     = 3
-  vm_id     = 200 + count.index
-  name      = "k0s-node-${count.index}"
-  node_name = var.proxmox_node_name
-
-  clone {
-    vm_id = proxmox_virtual_environment_vm.k0s_template.vm_id
-    full  = true # Essential for local-lvm compatibility
-  }
-
-  agent { enabled = true }
-  cpu   { cores = 2 }
-  memory { dedicated = 2048 }
-
-  network_device {
-    bridge = var.network_bridge
-  }
-
-  initialization {
-    datastore_id = "local-lvm"
-    dns {
-      servers = ["192.168.50.10"]
-    }
-    ip_config {
-      ipv4 {
-        # Results in .20, .21, .22
-        address = "192.168.80.${20 + count.index}/24"
-        gateway = "192.168.80.1"
+  # Setup the disk
+  disks {
+    ide {
+      ide3 {
+        cloudinit {
+          storage = "local-lvm"
+        }
       }
     }
-    user_account {
-      username = "ubuntu"
-      keys     = [var.ssh_public_key]
+    virtio {
+      virtio0 {
+        disk {
+          size         = 40
+          storage      = "local-lvm"
+          discard      = true
+        }
+      }
     }
   }
-}
 
+  # Setup the network interface and assign a vlan tag: 256
+  network {
+    id = 0
+    model  = "virtio"
+    bridge = "vmbr80"
+  }
+
+  # Setup the ip address using cloud-init.
+  boot = "order=virtio0"
+  # Keep in mind to use the CIDR notation for the ip.
+  ipconfig0 = "ip=192.168.80.20/24,gw=192.168.80.1"
+
+  sshkeys = <<EOF
+    ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCwp2LzcPGhJ3or+08v+YESykwuuMGnvU2/nNcU2ZBLGVOqErXUom+Q+ForBTtquohmMs1RpKB2PryYAssYoPG9nEwcJWZOB3YPL4XTjh9IgeZaQnm/fr6y4qYHp5+MAdePGHMiKse/yE0uUWra8kUiYe2KgkpkukddvxaCjA5LI75+/9VAtuNM2dIWrEUfZ7R6s/eE8iQ2faIfMyTU5QOUNI5/goUflMyP3gaIxi8nUZ9jkfj09agRSKl2dwamatbzR3IiAqxkaWaTk01ctOwSxW3vsH2GuHx5MS45ipSDv7ppBYh8oLuJCE1vVbM2TbBgF/j0vyVp0JV3w7JqTS0dh7hkMSekLiNTj/KfovymUnlo1Xq7b56cD8nvh2bXVqo/IiH7ArQ7vIoabSaBUPllGeF/BkvVQqYIBwEiKiwc3vcj6R45dXV96VpfAq382s675+b5jLRFGBGqAJBgpogflE+Vrs/1cV0cT7mOHFYbnvaOSf28OBHANMMuS8tGbTh9xMW/AgCSc7ZK31g6B3wOZbYxrxptR8nax3XnFXAYWjcEE2U8eS+P0LE/UltLNFnupz1+l9u5IcJbqwCrr8qV0CZwffaQL3+Yo+7kMQ2Uw7Te1BHvs0C8vKWfTzQ0XjwujvaN+lQdiEhTcT5nUTOufkvXfyfs77TlzOjNTiJUBQ== ri@ri-t-1005
+    EOF
+}
